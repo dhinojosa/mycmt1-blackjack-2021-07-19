bd6b79b710a94bd457cac41747e7cb423fb090bf
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2021-07-22 11:01:56 -0600

    Repaird Hand Value Test

--- src/test/java/com/jitterted/ebp/blackjack/HandValueAceTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/HandValueAceTest.java
@@ -4,11 +4,11 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class HandValueAceTest {
 
-    private Suit DUMMY_SUIT = Suit.HEARTS;
+    private final Suit DUMMY_SUIT = Suit.HEARTS;
 
     @Test
     public void handWithOneAceTwoCardsIsValuedAt11() throws Exception {
@@ -16,7 +16,7 @@ public class HandValueAceTest {
                                    new Card(DUMMY_SUIT, "5"));
 
         Hand hand = new Hand(cards);
-        assertThat(hand.hasValueOf(11+5)).isTrue();
+        assertThat(hand.hasValueOf(11 + 5)).isTrue();
     }
 
     @Test
@@ -28,5 +28,27 @@ public class HandValueAceTest {
         Hand hand = new Hand(cards);
         assertThat(hand.hasValueOf(1 + 8 + 3)).isTrue();
     }
+
+    //Ace + 10 or Face Card (J/Q/K) should result in a total of 21
+
+
+    @Test
+    void testAceAndFaceShouldBe21() {
+        List<Card> cards = List.of(new Card(DUMMY_SUIT, "A"),
+                                   new Card(DUMMY_SUIT, "Q"));
+        Hand hand = new Hand(cards);
+        assertThat(hand.hasValueOf(21)).isTrue();
+    }
+
+    @Test
+    void testAceAndValueCardShouldBe13() {
+        List<Card> cards = List.of(new Card(DUMMY_SUIT, "A"),
+                                   new Card(DUMMY_SUIT, "5"),
+                                   new Card(DUMMY_SUIT, "7"));
+
+        Hand hand = new Hand(cards);
+        assertThat(hand.hasValueOf(13)).isTrue();
+    }
+
 
 }
