80edd3ad3806b4d60d3ef331eed7497b9ea48a93
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2021-07-22 12:30:48 -0600

    Finished Lab 7

--- src/main/java/com/jitterted/ebp/blackjack/Wallet.java
+++ src/main/java/com/jitterted/ebp/blackjack/Wallet.java
@@ -1,4 +1,5 @@
 package com.jitterted.ebp.blackjack;
+
 //CMD_SHIFT_T = Toggle Prod & Test
 //F2 = Go to red line
 //OPT_ENTER = Fix
@@ -7,15 +8,29 @@ public class Wallet {
     private int balance;
 
     public boolean isEmpty() {
-        return balance == 0;
+        return this.balance == 0;
     }
 
     public void addMoney(int amount) {
-        if (amount < 0) throw new IllegalArgumentException();
+        ensureAmountIsPositive(amount);
         this.balance += amount;
     }
 
+    private void ensureAmountIsPositive(int amount) {
+        if (amount < 0) throw new IllegalArgumentException();
+    }
+
     public int getBalance() {
-        return balance;
+        return this.balance;
+    }
+
+    public void bet(int amount) {
+        ensureAmountIsPositive(amount);
+        ensureAmountDoesntExceedBalance(amount);
+        this.balance -= amount;
+    }
+
+    private void ensureAmountDoesntExceedBalance(int amount) {
+        if (amount > this.balance) throw new IllegalArgumentException();
     }
 }
--- src/test/java/com/jitterted/ebp/blackjack/WalletTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/WalletTest.java
@@ -1,6 +1,5 @@
 package com.jitterted.ebp.blackjack;
 
-import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -49,6 +48,49 @@ public class WalletTest {
     void testThatWeCannotPutNegativeIntoAddMoney() {
         Wallet wallet = new Wallet();
         assertThatThrownBy(() -> wallet.addMoney(-40))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void testWalletWith12Bet8BalanceIs4() {
+        Wallet wallet = new Wallet();
+        wallet.addMoney(12);
+        wallet.bet(8);
+        assertThat(wallet.getBalance()).isEqualTo(4);
+    }
+
+
+    @Test
+    void testWalletWith27Bet7AndBet9BalanceIs11() {
+        Wallet wallet = new Wallet();
+        wallet.addMoney(27);
+        wallet.bet(7);
+        wallet.bet(9);
+        assertThat(wallet.getBalance()).isEqualTo(11);
+    }
+
+
+    @Test
+    void testWalletWithMoneyWhenBetInFullIsEmpty() {
+        Wallet wallet = new Wallet();
+        wallet.addMoney(33);
+        wallet.bet(33);
+        assertThat(wallet.isEmpty()).isTrue();
+    }
+
+    @Test
+    void testBetMoreThanBalanceThrowsAnException() {
+        Wallet wallet = new Wallet();
+        wallet.addMoney(73);
+        assertThatThrownBy(() -> wallet.bet(74))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void testThatWeCantBetANegativeAmount() {
+        Wallet wallet = new Wallet();
+        wallet.addMoney(73);
+        assertThatThrownBy(() -> wallet.bet(-4))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
